0390f1ac4b393bfc2b5ec2bbdbff02d572d91cf7
Egggggg <mrhumanman2@gmail.com>
2023-03-10 22:27:03 -0500

    skull

--- src/ppu.rs
+++ src/ppu.rs
@@ -6,6 +6,8 @@ pub struct Ppu {
     window: Option<Window>,
     lcdc: u8,
     stat: u8,
+    line: u16,
+    fb: [u32; 256 * 256],
 }
 
 enum AddressType {
@@ -23,24 +25,31 @@ impl Ppu {
         };
         let lcdc = 0;
         let stat = 0;
+        let line = 0;
+        let fb = [0; 256 * 256];
 
-        let i = Self::interleave([0b11110000, 0b10101010]);
-
-        for e in i {
-            println!("{e:#010b}");
+        Self {
+            window,
+            lcdc,
+            stat,
+            line,
+            fb,
         }
-
-        Self { window, lcdc, stat }
     }
 
     pub fn new_headless() -> Self {
+        let window = None;
         let lcdc = 0;
         let stat = 0;
+        let line = 0;
+        let fb = [0; 256 * 256];
 
         Self {
-            window: None,
+            window,
             lcdc,
             stat,
+            line,
+            fb,
         }
     }
 
@@ -61,31 +70,26 @@ impl Ppu {
             // lightening shades of green
             let palette: [u32; 4] = [0x00002200, 0x000D2F0D, 0x00D0F2D0, 0x00DDFFDD];
 
-            // frame buffer to pass to window
-            let mut fb: [u32; 256 * 256] = [0; 256 * 256];
-
             for x in 0..32 {
-                for y in 0..32 {
-                    let offset: u16 = x * 32 + y;
-                    let tile_addr = match address_type {
-                        AddressType::Unsigned => 0x8000 + offset,
-                        AddressType::Signed => 0x9000_u16.wrapping_add(offset as i16 as u16),
-                    };
+                let offset: u16 = x * 32 + self.line;
+                let tile_addr = match address_type {
+                    AddressType::Unsigned => 0x8000 + offset,
+                    AddressType::Signed => 0x9000_u16.wrapping_add(offset as i16 as u16),
+                };
 
-                    // get byte pairs
-                    for i in 0..8 {
-                        let pair = memory.load_block(tile_addr, tile_addr + 1);
-                        let pixels = Self::interleave([pair[0], pair[1]]);
+                // get byte pairs
+                for i in 0..8 {
+                    let pair = memory.load_block(tile_addr, tile_addr + 1);
+                    let pixels = Self::interleave([pair[0], pair[1]]);
 
-                        for (j, pixel) in pixels.iter().enumerate() {
-                            fb[offset as usize + i as usize * 8 + j as usize] =
-                                palette[*pixel as usize];
-                        }
+                    for (j, pixel) in pixels.iter().enumerate() {
+                        let idx = offset as usize * 64 + i as usize * 8 + j as usize;
+                        self.fb[idx] = palette[*pixel as usize];
                     }
                 }
             }
 
-            window.update_with_buffer(&fb, 256, 256).unwrap();
+            window.update_with_buffer(&self.fb, 256, 256).unwrap();
         }
     }
 
@@ -94,14 +98,6 @@ impl Ppu {
         let mut out = [0; 8];
 
         for i in 0..8 {
-            /// 0x80
-            /// 0x40
-            /// 0x20
-            /// 0x10
-            /// 0x08
-            /// 0x04
-            /// 0x02
-            /// 0x01
             let high = (bytes[0] & (0x80 >> i)) << 1;
             let low = bytes[1] & (0x80 >> i);
 
